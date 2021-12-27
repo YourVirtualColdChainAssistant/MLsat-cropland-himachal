c9689666696951b245282da16576ea0a53a5de4c
danyayay <Ldy19970722>
2021-12-27 13:45:06 +0100

    Added spatial features with all bands

--- src/data/engineer.py
+++ src/data/engineer.py
@@ -109,9 +109,11 @@ def get_raw_every_n_weeks(logger, bands_name, n_weeks, bands_array, n=4):
     for i in np.arange(0, n_weeks, n):
         new_col_name = [n + '_' + str(i + 1) for n in bands_name]
         if n_weeks - i >= n:
-            df_new[new_col_name] = bands_array[:, :, i:i + n].max(axis=2)  # take max
+            df_new = pd.concat([df_new, pd.DataFrame(bands_array[:, :, i:i + n].max(axis=2), columns=new_col_name)],
+                               axis=1)  # take max
         else:
-            df_new[new_col_name] = bands_array[:, :, i:].max(axis=2)  # take max
+            df_new = pd.concat([df_new, pd.DataFrame(bands_array[:, :, i:].max(axis=2), columns=new_col_name)],
+                               axis=1)  # take max
     logger.info(f'  ok, {df_new.shape[1]} new features are added.')
     return df_new.copy()
 
@@ -131,7 +133,7 @@ def get_statistics_by_band(band_name, bands_array):
     df_new = pd.DataFrame()
     for op in ['avg', 'std', 'max']:
         col_name = band_name + '_' + op
-        df_new[col_name] = compute_statistics(op, bands_array)
+        df_new = pd.concat([df_new, pd.DataFrame(compute_statistics(op, bands_array), columns=[col_name])], axis=1)
     return df_new
 
 
@@ -148,7 +150,9 @@ def get_statistics(logger, bands_name, bands_array):
 def get_difference_by_band(band_name, n_weeks, bands_array):
     df_new = pd.DataFrame()
     for i in range(1, n_weeks):
-        df_new[band_name + '_diff_' + str(i)] = bands_array[:, i] - bands_array[:, i - 1]
+        col_name = band_name + '_diff_' + str(i)
+        df_new = pd.concat([df_new, pd.DataFrame(bands_array[:, i] - bands_array[:, i - 1], columns=[col_name])],
+                           axis=1)
         # fragmented df, please use pd.concat()
     return df_new
 
@@ -176,3 +180,68 @@ def get_difference(logger, bands_name, n_weeks, bands_array):
     df_new = pd.concat(df_new_list, axis=1)
     logger.info(f'  ok, {df_new.shape[1]} new features are added.')
     return df_new.copy()
+
+
+def get_spatial_features(logger, arr):
+    """
+
+    Parameters
+    ----------
+    logger
+    arr：np.array of ndvi time series
+        shape (height, width, n_weeks)
+
+    Returns
+    -------
+
+    """
+    logger.info('Adding spatial features of NDVI')
+    height, width, n_weeks = arr.shape
+    mean_list, std_list = [], []
+    for r in range(height):
+        for c in range(width):
+            neighbors = get_neighbors(arr, r, c)
+            mean_list.append(neighbors.mean(axis=(0, 1)))
+            std_list.append(neighbors.std(axis=(0, 1)))
+    df_mean = pd.DataFrame(mean_list, columns=['ndvi_spat_mean_' + str(w) for w in range(n_weeks)])
+    df_std = pd.DataFrame(std_list, columns=['ndvi_spat_std_' + str(w) for w in range(n_weeks)])
+    df = pd.concat([df_mean, df_std], axis=1)
+    logger.info(f"  ok, {df.shape[1]} spatial features are added.")
+    return df
+
+
+def get_all_spatial_features(logger, bands_name, arr):
+    logger.info('Adding all spatial features')
+    height, width, _, n_weeks = arr.shape
+    df = pd.DataFrame()
+    for i, b in enumerate(bands_name): 
+        mean_list, std_list = [], []
+        for r in range(height):
+            for c in range(width):
+                neighbors = get_neighbors(arr[:, :, i, :], r, c)
+                mean_list.append(neighbors.mean(axis=(0, 1)))
+                std_list.append(neighbors.std(axis=(0, 1)))
+        df_mean = pd.DataFrame(mean_list, columns=[b + '_spat_mean_' + str(w) for w in range(n_weeks)])
+        df_std = pd.DataFrame(std_list, columns=[b + '_spat_std_' + str(w) for w in range(n_weeks)])
+        df = pd.concat([df, df_mean, df_std], axis=1)
+    logger.info(f"  ok, {df.shape[1]} spatial features are added.")
+    return df
+
+
+def get_neighbors(array, row, col, radius=1):
+    height, width = array.shape[0], array.shape[1]
+    if row <= radius - 1:
+        row_start, row_end = 0, row + radius + 1
+    elif row >= height - radius:
+        row_start, row_end = row - radius, None
+    else:
+        row_start, row_end = row - radius, row + radius + 1
+    if col <= radius - 1:
+        col_start, col_end = 0, col + radius + 1
+    elif col == width - radius:
+        col_start, col_end = col - radius, None
+    else:
+        col_start, col_end = col - radius, col + radius + 1
+    # TODO: return neighbors + the point itself
+    neighbors = array[row_start:row_end, col_start:col_end]
+    return neighbors
